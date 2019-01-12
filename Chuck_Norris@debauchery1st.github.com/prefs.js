b290ab6e7012ee17f98ecf50c90c7af8c2191717
debauchery1st <trevor.j.martin@gmail.com>
2019-01-11 23:18:59 -0500

    GPL link in settings

--- Chuck_Norris@debauchery1st.github.com/prefs.js
+++ Chuck_Norris@debauchery1st.github.com/prefs.js
@@ -7,6 +7,14 @@ const _ = imports.gettext.domain(Me.uuid).gettext;
 const SETTINGS_NERDY_CONTENT = 'nerdy-content';
 const SETTINGS_EXPLICIT_CONTENT = 'explicit-content';
 
+const LICENSE_BRIEF = '\nChuck Norris GNOME shell extension\n\n' +
+    'version '+ Me.version + '\n\n' +
+    'jokes from <a href="http://www.icndb.com/">The Internet Chuck Norris Database</a>\n\n' +
+    'this program comes with ABSOLUTELY NO WARRANTY.\n' +
+    'This is free software, and you are welcome to redistribute it\n' +
+    '<a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0.html">See the ' +
+    'GNU General Pulic License, version 2 or later for details</a>';
+
 let settings;
 
 function init() {
@@ -21,34 +29,46 @@ function init() {
 }
 
 function buildPrefsWidget() {
-    let nerdyLabel = new Gtk.Label({
+    this._image = new Gtk.Image({
+        file: Me.path + "/icons/old/norris-lite.svg"
+    });
+    this._license = new Gtk.Label({
+        xalign: true, label: ""});
+    this._license.set_markup(LICENSE_BRIEF);
+    this._nerdy_label = new Gtk.Label({
         xalign: 1, label: _("Nerdy:")});
-    let nerdySwitch = new Gtk.Switch({
+    this._nerdy_switch = new Gtk.Switch({
         hexpand: false,
         active: settings.get_boolean(SETTINGS_NERDY_CONTENT)});
 
-    nerdySwitch.connect("notify::active", function (button) {
+    this._nerdy_switch.connect("notify::active", function (button) {
         settings.set_boolean(SETTINGS_NERDY_CONTENT, button.active);
     });
-    nerdySwitch.set_tooltip_text(_("Nerdy Jokes"));
+    this._nerdy_switch.set_tooltip_text(_("Nerdy Jokes"));
 
-    let nsfwLabel = new Gtk.Label({
+    this._nsfw_label = new Gtk.Label({
         xalign: 1, label: _("Explicit Content:")});
-    let nsfwSwitch = new Gtk.Switch({
+    this._nsfw_switch = new Gtk.Switch({
         hexpand: false,
         active: settings.get_boolean(SETTINGS_EXPLICIT_CONTENT)});
 
-    nsfwSwitch.connect("notify::active", function (button) {
+    this._nsfw_switch.connect("notify::active", function (button) {
         settings.set_boolean(SETTINGS_EXPLICIT_CONTENT, button.active);
     });
-    nsfwSwitch.set_tooltip_text(_("WARNING! N.S.F.W."));
+    this._nsfw_switch.set_tooltip_text(_("WARNING! N.S.F.W."));
 
-    let grid = new Gtk.Grid({column_spacing: 25, halign: Gtk.Align.CENTER, margin: 10, row_spacing: 10 });
-    grid.set_border_width(15);
-    grid.attach(nerdyLabel, 0, 1, 1, 1);
-    grid.attach(nerdySwitch, 1, 1, 1, 1);
-    grid.attach(nsfwLabel, 0, 2, 1, 1);
-    grid.attach(nsfwSwitch, 1, 2, 1, 1);
-    grid.show_all();
-    return grid;
+    this._grid = new Gtk.Grid({
+        column_spacing: 25,
+        halign: Gtk.Align.CENTER,
+        margin: 10, row_spacing: 10 });
+    this._grid.set_border_width(15);
+    this._grid.attach(this._nerdy_label, 1, 1, 1, 1);
+    this._grid.attach(this._nerdy_switch, 2, 1, 1, 1);
+    this._grid.attach(this._nsfw_label, 1, 2, 1, 1);
+    this._grid.attach(this._nsfw_switch, 2, 2, 1, 1);
+    this._grid.attach(this._image, 1, 3, 1, 1)
+    this._grid.attach(this._license, 1, 4, 1, 1);
+    this._grid.show_all();
+    return this._grid;
 }
+
